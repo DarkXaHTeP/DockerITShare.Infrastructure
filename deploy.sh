96ed59cd13f5863a03a79445b199cd274afd3372
Volodymyr Osmak <darkxahtep@gmail.com>
2019-03-25 23:39:38 +0200

    Added env replacing to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
+echo "Replacing env variables in compose"
+envsubst < docker-compose.yml > docker-compose-replaced.yml
+
 echo "Copying docker-compose file to server"
-sshpass -e scp -o StrictHostKeyChecking=no -r ./docker-compose.yml $SSHUSER@$SERVER_IP:/home/$SSHUSER/docker-compose-new.yml
+sshpass -e scp -o StrictHostKeyChecking=no -r ./docker-compose-replaced.yml $SSHUSER@$SERVER_IP:/home/$SSHUSER/docker-compose-new.yml
 
 echo "Running bootstrap on server"
 sshpass -e ssh -o StrictHostKeyChecking=no $SSHUSER@$SERVER_IP 'bash -s' < bootstrap.sh
