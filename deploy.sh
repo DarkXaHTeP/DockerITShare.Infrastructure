66a84ed3661c03f3339885eed8f14d3f9a166ccc
Volodymyr Osmak <darkxahtep@gmail.com>
2019-03-25 02:17:26 +0200

    Added strict host key checking option

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Copying docker-compose file to server"
-sshpass -e scp -r ./docker-compose.yml $SSHUSER@$SERVER_IP:/home/$SSHUSER/docker-compose-new.yml
+sshpass -e scp -o StrictHostKeyChecking=no -r ./docker-compose.yml $SSHUSER@$SERVER_IP:/home/$SSHUSER/docker-compose-new.yml
 
 echo "Running bootstrap on server"
-sshpass -e ssh $SSHUSER@$SERVER_IP 'bash -s' < bootstrap.sh
+sshpass -e ssh -o StrictHostKeyChecking=no $SSHUSER@$SERVER_IP 'bash -s' < bootstrap.sh
