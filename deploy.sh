cb6929003c09efd7eb2293e648cb64bc78a5ed7d
Volodymyr Osmak <darkxahtep@gmail.com>
2019-04-15 01:45:41 +0300

    Added filebeat

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 echo "Replacing env variables in compose"
 envsubst < docker-compose.yml > docker-compose-replaced.yml
 
-echo "Copying docker-compose file to server"
+echo "Copying docker-compose files to server"
 sshpass -e scp -o StrictHostKeyChecking=no -r ./docker-compose-replaced.yml $SSHUSER@$SERVER_IP:/home/$SSHUSER/docker-compose-new.yml
+sshpass -e scp -o StrictHostKeyChecking=no -r ./filebeat.yml $SSHUSER@$SERVER_IP:/home/$SSHUSER/filebeat.yml
 
 echo "Running bootstrap on server"
 sshpass -e ssh -o StrictHostKeyChecking=no $SSHUSER@$SERVER_IP 'bash -s' < bootstrap.sh
